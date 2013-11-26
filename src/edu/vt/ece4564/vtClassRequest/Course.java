c6d8be804b6bc36c60850216989fffb2a144cc81
Brian Hession <hessionb@vt.edu>
2013-11-26 16:58:58 -0500

    Updated course and timetablescraper. added coursetime

--- src/edu/vt/ece4564/vtClassRequest/Course.java
+++ src/edu/vt/ece4564/vtClassRequest/Course.java
@@ -2,32 +2,25 @@ package edu.vt.ece4564.vtClassRequest;
 
 import java.io.Serializable;
 
-public class Course implements Serializable{
-
-	public static final int MONDAY = 1;
-	public static final int TUESDAY = 2;
-	public static final int WEDNESDAY = 3;
-	public static final int THURSDAY = 4;
-	public static final int FRIDAY = 5;
+public class Course implements Serializable {
+	
+	private static final long serialVersionUID = 8026434170877104021L;
 
 	private String name;
 	private String subj;
 	private String crse;
 	private String crn;
+	private CourseTime time;
 	private String location;
-	private int startTime;
-	private int endTime;
-	private int days[];
-	private int additionalStartTime;
-	private int additionalEndTime;
-	private int additionalDays[];
+	private CourseTime additionalTime;
 	private String additionalLocation;
 	private String instructor;
 	private Course prereqs[];
 	private String classRestrictions;
 	private int credits;
 	private int id;
-
+	private String type;
+	
 	public Course() {
 		// Initialize unnecessary stuff
 		this.name = null;
@@ -35,76 +28,58 @@ public class Course implements Serializable{
 		this.crse = null;
 		this.credits = -1;
 		this.crn = null;
-		this.startTime = -1;
-		this.endTime = -1;
-		this.days = null;
+		this.time = null;
 		this.location = null;
-		this.additionalDays = null;
-		this.additionalStartTime = -1;
-		this.additionalEndTime = -1;
+		this.additionalTime = null;
 		this.additionalLocation = null;
 		this.instructor = null;
 		this.prereqs = null;
 		this.classRestrictions = null;
 	}
-
+	
+	public Course(String subj, String crse) {
+		this.subj = subj;
+		this.crse = crse;
+		
+		// Initialize unnecessary stuff
+		this.name = null;
+		this.credits = -1;
+		this.crn = null;
+		this.location = null;
+		this.additionalTime = null;
+		this.additionalLocation = null;
+		this.instructor = null;
+		this.prereqs = null;
+		this.classRestrictions = null;
+	}
+	
 	public Course(String name, String subj, String crse, int credits) {
 		this.name = name;
 		this.subj = subj;
 		this.crse = crse;
 		this.credits = credits;
-
+		
 		// Initialize unnecessary stuff
 		this.crn = null;
-		this.startTime = -1;
-		this.endTime = -1;
-		this.days = null;
 		this.location = null;
-		this.additionalDays = null;
-		this.additionalStartTime = -1;
-		this.additionalEndTime = -1;
+		this.additionalTime = null;
 		this.additionalLocation = null;
 		this.instructor = null;
 		this.prereqs = null;
 		this.classRestrictions = null;
 	}
-
-	   public Course(String subj, String crse) {
-	        this.name = null;
-	        this.subj = subj;
-	        this.crse = crse;
-	        this.credits = -1;
-
-	        // Initialize unnecessary stuff
-	        this.crn = null;
-	        this.startTime = -1;
-	        this.endTime = -1;
-	        this.days = null;
-	        this.location = null;
-	        this.additionalDays = null;
-	        this.additionalStartTime = -1;
-	        this.additionalEndTime = -1;
-	        this.additionalLocation = null;
-	        this.instructor = null;
-	        this.prereqs = null;
-	        this.classRestrictions = null;
-	    }
-
-	public Course(String name, String subj, String crse, String crn, int credits, int startTime, int endTime, int[] days) {
+	
+	public Course(String name, String subj, String crse, String crn, int credits, CourseTime time) {
 		this.name = name;
 		this.subj = subj;
 		this.crse = crse;
 		this.crn = crn;
 		this.credits = credits;
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.days = days;
-
+		this.time = time;
+		
 		// Initialize unnecessary stuff
 		this.location = null;
-		this.additionalDays = null;
-		this.additionalStartTime = -1;
-		this.additionalEndTime = -1;
+		this.additionalTime = null;
 		this.additionalLocation = null;
 		this.instructor = null;
 		this.prereqs = null;
@@ -182,7 +157,7 @@ public class Course implements Serializable{
 	public void setAdditionalLocation(String additionalLocation) {
 		this.additionalLocation = additionalLocation;
 	}
-
+	
 	public String getCrse() {
 		return crse;
 	}
@@ -199,99 +174,55 @@ public class Course implements Serializable{
 		this.crn = crn;
 	}
 
-	public int getStartTime() {
-		return startTime;
+	public String getType() {
+		return type;
 	}
 
-	public void setStartTime(int startTime) {
-		this.startTime = startTime;
+	public void setType(String type) {
+		this.type = type;
 	}
 
-	public int getEndTime() {
-		return endTime;
+	public CourseTime getTime() {
+		return time;
 	}
 
-	public void setEndTime(int endTime) {
-		this.endTime = endTime;
+	public void setTime(CourseTime time) {
+		this.time = time;
 	}
 
-	public int[] getDays() {
-		return days;
+	public CourseTime getAdditionalTime() {
+		return additionalTime;
 	}
 
-	public void setDays(int[] days) {
-		this.days = days;
-	}
-
-	public int getAdditionalStartTime() {
-		return additionalStartTime;
-	}
-
-	public void setAdditionalStartTime(int additionalStartTime) {
-		this.additionalStartTime = additionalStartTime;
-	}
-
-	public int getAdditionalEndTime() {
-		return additionalEndTime;
-	}
-
-	public void setAdditionalEndTime(int additionalEndTime) {
-		this.additionalEndTime = additionalEndTime;
-	}
-
-	public int[] getAdditionalDays() {
-		return additionalDays;
-	}
-
-	public void setAdditionalDays(int[] additionalDays) {
-		this.additionalDays = additionalDays;
+	public void setAdditionalTime(CourseTime additionalTime) {
+		this.additionalTime = additionalTime;
 	}
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder("Course: " + name + "\n" +
 											 subj + "-" + crse + "\n" +
 											 "CRN: " + crn + "\n");
-
-		if(startTime != -1 && endTime != -1 && days != null) {
-			sb.append(daysToString(days) + " " + startTime / 60 + ":" + startTime % 60 + "-" + endTime / 60 + ":" + endTime % 60 + "\n");
+		
+		if(type != null && !type.startsWith("O")) {
+			if(time != null) {
+				sb.append(time.toString() + "\n");
+			}
+			
+			if(additionalTime != null) {
+				sb.append("Additional Times:" + additionalTime.toString() + "\n");
+			}
 		}
-
-		if(additionalStartTime != -1 && additionalEndTime != -1 && additionalDays != null) {
-			sb.append("Additional Times: " + daysToString(additionalDays) + " " + additionalStartTime / 60 + ":" +
-					additionalStartTime % 60 + "-" + additionalEndTime / 60 + ":" + additionalEndTime % 60 + "\n");
+		else if(type != null) {
+			sb.append("Online class\n");
 		}
-
+		
 		if(location != null) {
 			sb.append("Location: " + location + "\n");
 		}
-
+		
 		sb.append("Instructor: " + instructor + "\n" +
 				  "Credits: " + credits);
-
-		return sb.toString();
-	}
-
-	private String daysToString(int[] days) {
-		StringBuilder sb = new StringBuilder();
-		for(int d : days) {
-			switch(d) {
-			case MONDAY:
-				sb.append("M ");
-				break;
-			case TUESDAY:
-				sb.append("T ");
-				break;
-			case WEDNESDAY:
-				sb.append("W ");
-				break;
-			case THURSDAY:
-				sb.append("R ");
-				break;
-			case FRIDAY:
-				sb.append("F ");
-				break;
-			}
-		}
+		
 		return sb.toString();
 	}
 }
